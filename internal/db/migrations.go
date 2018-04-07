d915fff5f7aca68eec6663852e07d6feaaede9db
AbeIgnocious <k.l.kubat@gmail.com>
2018-04-07 21:16:05 +0200

    create migrations file

---
+++ internal/db/migrations.go
@@ -0,0 +1,62 @@
+package db
+
+import (
+	"github.com/getsentry/raven-go"
+	"github.com/jinzhu/gorm"
+
+)
+
+func Migrate(Local gorm.DB, Remote gorm.DB){
+	// migrations are only performed by GORM if a table/column/index does not exist.
+	err := Local.AutoMigrate(&BinanceMarket{}, &BinanceTicker{}, &BinanceOrder{}, &BinanceOrderBook{}).Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+	err = Local.Exec("CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;").Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+
+	err = Local.Exec("CREATE EXTENSION IF NOT EXISTS dblink;").Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+	err = Local.Exec("SELECT create_hypertable('bitfinex_orders', 'time',  'orderbook_id', if_not_exists => TRUE)").Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+	err = Local.Exec("SELECT create_hypertable('bitfinex_tickers', 'time', 'market_id', if_not_exists => TRUE)").Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+	err =Local.Exec("SELECT create_hypertable('bitfinex_order_books', 'time', 'market_id', if_not_exists => TRUE)").Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+
+	err = Remote.AutoMigrate(&BinanceMarket{}, &BinanceOrder{}, &BinanceTicker{}, &BinanceOrderBook{}).Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+	err = Remote.Exec("CREATE EXTENSION IF NOT EXISTS dblink;").Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+
+	err = Remote.Exec("CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;").Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+	err = Remote.Exec("SELECT create_hypertable('bitfinex_orders', 'time',  'orderbook_id', if_not_exists => TRUE)").Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+	err = Remote.Exec("SELECT create_hypertable('bitfinex_tickers', 'time', 'market_id', if_not_exists => TRUE)").Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+	err =Remote.Exec("SELECT create_hypertable('bitfinex_order_books', 'time', 'market_id', if_not_exists => TRUE)").Error
+	if err != nil{
+		raven.CaptureErrorAndWait(err, nil)
+	}
+}
